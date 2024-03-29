02156734b12b7628f1e306c37f5e9e3f3c5f1b50
Kevin Avila <kevinavila98787@gmail.com>
2024-03-28 22:41:21 -0600

    first part

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,34 @@
+
+const Paciente = () => {
+  return (
+    <>
+    <div className="mx-5 bg-white shadow-md px-5 py-10 rounded-xl my-10">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propetario: {''}
+            <span className="font-normal normal-case">Juan</span>
+            </p>
+
+            <p classNamex="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+            <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de Alta: {''}
+            <span className="font-normal normal-case">10 Diciembre de 2022</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+            <span className="font-normal normal-case">Fiebre Alta y sida</span>
+            </p>
+
+            
+
+        </div>
+
+        </>
+  )
+}
+
+export default Paciente
